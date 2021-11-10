da943810c5e776c463d9775f11541601db9c9684
krashLanded <ericquesada02@icloud.com>
2021-11-09 22:22:57 -0800

    Added event listener to button

--- hub.js
+++ hub.js
@@ -1,6 +1,14 @@
 const container = document.querySelector('.container');
 const buttonStart = document.createElement('button');
 
+
+
 buttonStart.classList.add('buttonStart');
 buttonStart.textContent = 'Click to start!';
 container.appendChild(buttonStart);
+
+
+buttonStart.addEventListener('click', () => {
+    buttonStart.remove();
+    alert('Hello!');
+});
