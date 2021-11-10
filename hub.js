8fdf2a3a5aef389069244ce38d7f038477854418
krashLanded <ericquesada02@icloud.com>
2021-11-09 22:11:52 -0800

    File creation

---
+++ hub.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const buttonStart = document.createElement('button');
+
+buttonStart.classList.add('buttonStart');
+buttonStart.textContent = 'Click to start!';
+container.appendChild(buttonStart);
