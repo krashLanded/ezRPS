c2eddc6f9b60dcf1dfe883b94e14e8554e634acf
krashLanded <ericquesada02@icloud.com>
2021-11-10 23:34:11 -0800

    Created button functionality

--- app.js
+++ app.js
@@ -4,7 +4,38 @@ const gameChoices = [
     "Scissors"
 ];
 
-let computerChoice = gameChoices[Math.floor(Math.random()*gameChoices.length)];
+
+function rockRoundPlay(playerMove, computerMove) {
+    if (playerMove == "rock" && computerMove == "rock") {
+        alert('Tie! They also chose rock.')
+    } else if (playerMove == "rock" && computerMove == "paper") {
+        alert('You lose. They chose paper!')
+    } else if (playerMove == "rock" && computerMove == "scissors") {
+        alert('You win! They chose scissors.')
+    }
+    }
+function paperRoundPlay(playerMove, computerMove) {
+    if (playerMove == "paper" && computerMove == "rock") {
+        alert('You win! They chose rock.')
+    } else if (playerMove == "paper" && computerMove == "paper") {
+        alert('Tie! They also chose paper.')
+    } else if (playerMove == "paper" && computerMove == "scissors") {
+        alert('You lose. They chose scissors!')
+    }
+    }
+function scissorsRoundPlay(playerMove, computerMove) {
+    if (playerMove == "scissors" && computerMove == "rock") {
+        alert('You lose. They chose rock!')
+    } else if (playerMove == "scissors" && computerMove == "paper") {
+        alert('You win! They chose paper.')
+    } else if (playerMove == "scissors" && computerMove == "scissors") {
+        alert('Tie! They also chose scissors.')
+    }
+    }
+    
+
+  
+
 
 
 
